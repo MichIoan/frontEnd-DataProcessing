0c0a3b2ab245fb0674901ee2330b362b21e6269b
Iarina <112045565+iarinamd@users.noreply.github.com>
2023-12-10 22:26:21 +0100

    created landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      {/* Netflix Navbar */}
+      <div className="navbar">
+        <img className="netflix-logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"/>
+      </div>
+
+      {/* Main Content */}
+      <div className="main-content">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <p>Watch anywhere. Cancel anytime.</p>
+
+        {/* Email Input */}
+        <div className="email-input">
+          <input type="email" placeholder="Enter your email" className="email-field"/>
+          <button className="button">Try it now</button>
+        </div>
+      </div>
+
+      {/* Netflix Footer */}
+      <div className="footer">
+        <p>Questions? Call 1-800-123-4567</p>
+        <div className="footer-links">
+          <span>FAQ</span>
+          <span>Help Center</span>
+          <span>Account</span>
+          <span>Media Center</span>
+          <span>Investor Relations</span>
+          <span>Jobs</span>
+          <span>Terms of Use</span>
+          <span>Privacy</span>
+          <span>Cookie Preferences</span>
+          <span>Corporate Information</span>
+          <span>Contact Us</span>
+        </div>
+        <p>&copy; 2023 Netflix, Inc.</p>
+      </div>
     </div>
   );
 }
